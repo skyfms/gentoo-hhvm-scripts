5b27a0e92d4547a3648567d01b33a1f08cf68817
Kristaps Kaupe <kristaps@blogiem.lv>
2015-01-07 13:50:51 +0200

    Unified service name parameter format

--- check_and_start_hhvm.sh
+++ check_and_start_hhvm.sh
@@ -1,23 +1,33 @@
 #! /bin/bash
 
 if [ "$1" == "" ]; then
-        echo "Usage: `basename $0` port"
+        echo "Usage: `basename $0` svcname"
         exit 1
 fi
 
-PORT=$1
-PID="`cat /var/run/hhvm/hhvm.${PORT}.pid`"
+SVCNAME=$1
+PID="`cat /var/run/hhvm/${SVCNAME}.pid`"
+
+if [[ "`find -L /etc/init.d/ -samefile /etc/init.d/hhvm | grep /etc/init.d/${SVCNAME}`" == "" ]]; then
+        # Allow short parameter as hhvm.$1
+        if [[ "`find -L /etc/init.d/ -samefile /etc/init.d/hhvm | grep /etc/init.d/hhvm.${SVCNAME}`" != "" ]]; then
+                SVCNAME=hhvm.$SVCNAME
+        else
+                echo "FATAL: $SVCNAME is not a HHVM service!"
+                exit 1
+        fi
+fi
 
 if [ "$PID" == "" ]; then
         echo No PID, starting up
-        /etc/init.d/hhvm.${PORT} restart
+        /etc/init.d/${SVCNAME} restart
 else
         if ! kill -0 $PID 2> /dev/null; then
                 echo HHVM PID $PID not running, starting up
                 # Stop, just in case, if crashed. Else you would get:
                 #  * WARNING: hhvm.83 has already been started
-                /etc/init.d/hhvm.${PORT} stop
-                /etc/init.d/hhvm.${PORT} start
+                /etc/init.d/${SVCNAME} stop
+                /etc/init.d/${SVCNAME} start
         fi
 fi
 
